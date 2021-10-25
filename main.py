220de4e10750856192a20aca7d248929fc07eff8
Benjamin <bwilson1@ualberta.ca>
2021-10-24 19:18:44 -0600

    created Node class and init function

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-# This is a sample Python script.
+class Node:
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+    def __init__(self, data, depth, cost):
+        '''Initializes the node class and fills the node with required data'''
 
+        self.data = data
+        self.depth = depth
+        self.cost = cost
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
